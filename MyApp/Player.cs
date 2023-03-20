71f821bdcb9c4a7f6de0217dced76527da35af47
ChetanM1555 <126090346+ChetanM1555@users.noreply.github.com>
2023-03-20 18:00:36 +0200

    game done

--- MyApp/Player.cs
+++ MyApp/Player.cs
@@ -7,39 +7,66 @@ namespace MyApp
 {
     public class Player
     {
-        public static char[] choice(char[] pos, char xo){
+        public static char[] choice(char[] pos, char xo)
+        {
             bool correct = false;
             do{
             Console.Write("Enter a digit between 1 and 9: ");
             ConsoleKeyInfo keyInfo = Console.ReadKey();
-            int digit = int.Parse(keyInfo.KeyChar.ToString());
-            char charValue = Convert.ToChar(digit);
-            Console.WriteLine();
-            // if (char.IsDigit(keyInfo.KeyChar) && diget != 0)
-            // {
-                
-                int index = Array.IndexOf(pos, charValue);
 
-                if (index <= 0)
+            if (char.IsDigit(keyInfo.KeyChar))
+            {
+                int digit = int.Parse(keyInfo.KeyChar.ToString());
+                char charValue = (char)(digit + '0');
+                int index = Array.IndexOf(pos, charValue);
+                                Console.WriteLine(index);
+                if (index >= 0)
                 {
-                    
-                    Console.WriteLine("You entered: " + digit + index);
+                    Console.WriteLine("You entered: " + digit);
                     pos[digit] = xo;
                     correct = true;
-
                 }
-                else {
+                else 
+                {
                     Console.WriteLine("That position was already picked");
-                    
                 }
-                
-            // }
-            // else
-            // {
-            //     Console.WriteLine("Invalid input. Please enter a digit between 1 and 9.");
-            // }
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a digit between 1 and 9.");
+            }
             }while (correct == false);
             return pos;
+        }
+
+        public static bool winCondition(char[] pos)
+        {
+            if (pos[1]==pos[2]&&pos[2]==pos[3]) {
+                return true;
+            }
+            if (pos[4]==pos[5]&&pos[5]==pos[6]) {
+                return true;
+            }
+            if (pos[7]==pos[8]&&pos[8]==pos[9]) {
+                return true;
+            }
+            if (pos[1]==pos[4]&&pos[4]==pos[7]) {
+                return true;
+            }
+            if (pos[2]==pos[5]&&pos[5]==pos[8]) {
+                return true;
+            }
+            if (pos[3]==pos[6]&&pos[6]==pos[9]) {
+                return true;
+            }
+            if (pos[1]==pos[5]&&pos[5]==pos[9]) {
+                return true;
+            }
+            if (pos[7]==pos[5]&&pos[5]==pos[3]) {
+                return true;
+            }
+            return false;
         }
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -6,30 +6,40 @@ using static MyApp.Player;
 class Program
 {
 static int player = 1;
-static char[] position = { '0', '1', 'X', '3', '4', '5', '6', '7', '8', '9' };
+static char[] position = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 static char[] xo = {'X','O'};
 static bool win = false;
+
 
 public static void Main(string[] args)
     {
         Console.WriteLine("Welcome to Tic-Tac-Toe!!");
         Console.WriteLine("Player1 is X and Player2 is O...");
         char place = 'X';
+        DrawBoard(position);
         do{
+            
             if (player%2 != 0)
             {
                 place = 'X';
-                
             }
             else{
                 place ='O';
             }
             Console.WriteLine(place);
-            DrawBoard(position);
             position = choice(position, place);
+            DrawBoard(position);
             player++;
+            win = winCondition(position);
             // win = true;
-        }while(win == false);
+        }while(win == false && player != 10);
+            if (win == true){
+                Console.WriteLine("Congrates "+place+"'s wins!!");
+            }
+            else{
+                Console.WriteLine("Out of plays, the game is a draw!!");
+
+            }
     }
 }
 
